38c055f61f78e67fe1f2753ead3c4bf40b1c3208
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2021-10-04 22:50:35 -0400

    Funcionalidad 2 -> ok - sum two numbers by comma

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,18 @@ import {sumString} from "./Functions";
 
 describe("--- SUM STRING --- ",() =>  {
     it("Test 1.1 - calculate one number",() => {
-        expect( sumString("2.1")).toEqual(2.1); 
+        expect( sumString("2")).toEqual(2); 
     } );
-    it("Test 1.2 - calculate one number",() => {
+    it("Test 1.2 - calculate one number - empty",() => {
         expect( sumString("")).toEqual("Formato incorrecto"); 
     } );
+    it("Test 2.1 - sum two numbers by comma",() => {
+        expect( sumString("2,1")).toEqual(3); 
+    });
+    it("Test 2.2 - sum two numbers by comma",() => {
+        expect( sumString("123")).toEqual(123); 
+    });
+    it("Test 2.2 - sum two numbers by comma",() => {
+        expect( sumString("1265478")).toEqual(1265478); 
+    });
 } )
--- src/Functions.js
+++ src/Functions.js
@@ -7,6 +7,23 @@ export function sumString(num_string){
     if (!num_string){
         return "Formato incorrecto";
     }
-    let num_int = Number(num_string);
-    return num_int;
+    if (num_string.length == 1){
+        return  Number(num_string);
+    }
+    let myArr_num = num_string.split(",").map(Number);
+
+/*
+    var b = num_string.split(',').map(function(item) {
+        return parseInt(item, 10);
+    });
+    return b;*/
+    return sum_array(myArr_num);
+}
+
+function sum_array(array_numbers){
+    var suma = 0;
+    array_numbers.forEach(function(numero){
+        suma += numero;
+    });
+    return suma;
 }
